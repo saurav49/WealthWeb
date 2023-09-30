47f5bf7b84a02d9b3ca9bf1504cc6b5369ab431c
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2023-09-30 14:06:53 +0530

    chore: setup main with swagger, validation, enable cors, url prefix added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { promises } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api/v1');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const pkg = JSON.parse(
+    await promises.readFile(join('.', 'package.json'), 'utf8'),
+  );
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion(pkg.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  const configService = app.get(ConfigService);
+  await app.listen(+configService.get('PORT'));
 }
 bootstrap();
