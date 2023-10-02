dec107d5480c814b8aaa24cff48188089fb38bdd
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2023-10-02 12:44:29 +0530

    feat: authservice created with reqd function, auth routes created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('/signin')
+  signInUser() {
+    this.authService.signInUser();
+  }
+
+  @Post('/signup')
+  signUpUser() {
+    this.authService.signUpUser();
+  }
+
+  @Post('/refresh')
+  refreshToken() {
+    this.authService.refreshToken();
+  }
+
+  @Post('/logout')
+  logoutUser() {
+    this.authService.logoutUser();
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyUserModule } from 'src/company-user/company-user.module';
 import { CompanyModule } from 'src/company/company.module';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CompanyUserModule, CompanyModule])],
   controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
